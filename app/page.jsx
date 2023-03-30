c888ba41339869fcc814fec50d22d4bb56645393
Dan <61760657+danielmustard@users.noreply.github.com>
2023-03-30 11:59:13 +0000

    added re-validate code so page changes are seen on live server

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import '../styles/all-posts.css'
 async function getAllPosts() {
   const NOTION_BLOG_ID = "02382c790ccb47aa8dd21892cdab2eb9"
   try {
-    const response = await fetch(`https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`);
+    const response = await fetch(`https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`, {next:{revalidate:60}});
     const data = await response.json();
     return data;
   } catch (error) {
--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -2,11 +2,9 @@ import { NotionRenderer } from "react-notion";
 import '../../../styles/blogpost.css'
 import "prismjs/themes/prism-tomorrow.css";
 async function getPageData(id){
-    const res = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`)
+    const res = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`,{next:{revalidate:60}})
     return res.json();
 }
-
-
 
 const page = async ({params}) => {
     const pageData = await getPageData(params.id)
